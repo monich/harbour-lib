9f91b79f34f75ca099c09819dc57d099b909c705
Slava Monich <slava.monich@jolla.com>
2017-11-21 17:57:45 +0200

    Updated HarbourPluginLoader to get around ABI break in Qt 5.6
    Perhaps all internal Qt5 functions should be loaded by name

--- src/HarbourPluginLoader.cpp
+++ src/HarbourPluginLoader.cpp
@@ -33,12 +33,57 @@
 #include "HarbourPluginLoader.h"
 #include "HarbourDebug.h"
 
+// These are included from qqmlengine.h
+#include <QUrl>
+#include <QObject>
+#include <QMap>
+#include <QJSEngine>
+#include <QQmlError>
+
+#if QT_VERSION >= 0x050000
+
+// Unprotect QQmlEngine::d_func
+#define private public
+#include <QQmlEngine>
+#undef private
+
 #include <qqml.h>
 #include <QQmlEngine>
 #include <QQmlComponent>
 
-// This hack allows (in some cases) to use prohibited QML imports by
+#include <dlfcn.h>
+
+// This hack allows (in most cases) to use prohibited QML imports by
 // re-registering them under a different name
+
+// These:
+// QQmlAttachedPropertiesFunc QQmlType::attachedPropertiesFunction() const
+// _ZNK8QQmlType26attachedPropertiesFunctionEv
+//
+// const QMetaObject* QQmlType::attachedPropertiesType() const
+// _ZNK8QQmlType22attachedPropertiesTypeEv
+//
+// have been replaced with these in Qt 5.6:
+//
+// QQmlAttachedPropertiesFunc QQmlType::attachedPropertiesFunction(QQmlEnginePrivate*) const
+// _ZNK8QQmlType26attachedPropertiesFunctionEP17QQmlEnginePrivate
+//
+// const QMetaObject* QQmlType::attachedPropertiesType(QQmlEnginePrivate*) const
+// _ZNK8QQmlType22attachedPropertiesTypeEP17QQmlEnginePrivate
+//
+
+#define LIBQT5QML_SO "/usr/lib/libQt5Qml.so.5"
+
+// sym,ret,name,args
+#define QMLTYPE_FUNCTIONS(f) \
+    f("_ZNK8QQmlType26attachedPropertiesFunctionEv", \
+        QQmlAttachedPropertiesFunc, AttachedPropertiesFunctionProc,(QQmlType*)) \
+    f("_ZNK8QQmlType22attachedPropertiesTypeEv", \
+        const QMetaObject*, AttachedPropertiesTypeProc,(QQmlType*)) \
+    f("_ZNK8QQmlType26attachedPropertiesFunctionEP17QQmlEnginePrivate", \
+        QQmlAttachedPropertiesFunc, AttachedPropertiesFunctionProc6, (QQmlType*,QQmlEnginePrivate*)) \
+    f("_ZNK8QQmlType22attachedPropertiesTypeEP17QQmlEnginePrivate", \
+        const QMetaObject*, AttachedPropertiesTypeProc6, (QQmlType*,QQmlEnginePrivate*))
 
 // PRIVATE QT API!
 class Q_QML_EXPORT QQmlType
@@ -64,6 +109,20 @@ public:
     static QQmlType* qmlType(const QString &qualifiedName, int, int);
 };
 
+static const char* QmlTypeSymbols[] = {
+#define QMLTYPE_SYMBOL(sym,ret,name,args) sym,
+    QMLTYPE_FUNCTIONS(QMLTYPE_SYMBOL)
+};
+
+typedef struct _QmlTypeFunctions {
+#define QMLTYPE_TYPEDEF(sym,ret,name,args) ret (*name) args;
+QMLTYPE_FUNCTIONS(QMLTYPE_TYPEDEF)
+} QmlTypeFunctions;
+
+#define _N(a) (sizeof(a)/sizeof((a)[0]))
+#define NUM_FUNCTIONS _N(QmlTypeSymbols)
+Q_STATIC_ASSERT(sizeof(QmlTypeFunctions) == NUM_FUNCTIONS*sizeof(void*));
+
 // ==========================================================================
 // HarbourPluginLoader::Private
 // ==========================================================================
@@ -83,11 +142,16 @@ public:
         const char* aModule, int aMajor, int aMinor);
 
 public:
-    bool iLoaded;
     QQmlEngine* iEngine;
     QString iModule;
     int iMajor;
     int iMinor;
+    bool iLoaded;
+    void* iHandle;
+    union {
+        QmlTypeFunctions fn;
+        void* ptr[NUM_FUNCTIONS];
+    } iLibQt5Qml;
 };
 
 HarbourPluginLoader::Private::Private(
@@ -95,18 +159,30 @@ HarbourPluginLoader::Private::Private(
     QString aModule,
     int aMajor,
     int aMinor) :
-    iLoaded(false),
     iEngine(aEngine),
     iModule(aModule),
     iMajor(aMajor),
-    iMinor(aMinor)
+    iMinor(aMinor),
+    iLoaded(false),
+    iHandle(NULL)
 {
+    memset(&iLibQt5Qml, 0, sizeof(iLibQt5Qml));
     // Load the actual import library
     QQmlComponent* component = new QQmlComponent(iEngine);
     component->setData(QString("import QtQuick 2.0\nimport %1 %2.%3\nQtObject {}").
         arg(iModule).arg(iMajor).arg(iMinor).toUtf8(), QUrl());
     if (component->status() == QQmlComponent::Ready) {
         delete component->create();
+        // Resolve unstable symbols
+        iHandle = dlopen(LIBQT5QML_SO, RTLD_LAZY);
+        if (iHandle) {
+            for (uint i = 0; i < NUM_FUNCTIONS; i++) {
+                iLibQt5Qml.ptr[i] = dlsym(iHandle, QmlTypeSymbols[i]);
+                HDEBUG(QmlTypeSymbols[i] << (iLibQt5Qml.ptr[i] ? "OK" : "missing"));
+            }
+        } else {
+            HWARN("Failed to load" << LIBQT5QML_SO);
+        }
         iLoaded = true;
     } else {
         HWARN(component->errors());
@@ -116,6 +192,9 @@ HarbourPluginLoader::Private::Private(
 
 HarbourPluginLoader::Private::~Private()
 {
+    if (iHandle) {
+        dlclose(iHandle);
+    }
 }
 
 QQmlType*
@@ -163,6 +242,19 @@ HarbourPluginLoader::Private::reRegisterType(
     int aMinor)
 {
     if (aType && iEngine) {
+        // Get around the ABI break in Qt 5.6
+        QQmlAttachedPropertiesFunc attachedPropertiesFunction =
+            iLibQt5Qml.fn.AttachedPropertiesFunctionProc ?
+            iLibQt5Qml.fn.AttachedPropertiesFunctionProc(aType) :
+            iLibQt5Qml.fn.AttachedPropertiesFunctionProc6 ?
+            iLibQt5Qml.fn.AttachedPropertiesFunctionProc6(aType, iEngine->d_func()) :
+            NULL;
+        const QMetaObject *attachedPropertiesMetaObject =
+            iLibQt5Qml.fn.AttachedPropertiesTypeProc ?
+            iLibQt5Qml.fn.AttachedPropertiesTypeProc(aType) :
+            iLibQt5Qml.fn.AttachedPropertiesTypeProc6 ?
+            iLibQt5Qml.fn.AttachedPropertiesTypeProc6(aType, iEngine->d_func()) :
+            NULL;
         QQmlPrivate::RegisterType type = {
             0, // int version;
             aType->typeId(),  // int typeId;
@@ -175,8 +267,8 @@ HarbourPluginLoader::Private::reRegisterType(
             aMinor, // int versionMinor;
             aQmlName, // const char *elementName;
             aType->metaObject(), // const QMetaObject *metaObject;
-            aType->attachedPropertiesFunction(NULL /*iEngine->d_func()*/),
-            aType->attachedPropertiesType(NULL /*iEngine->d_func()*/),
+            attachedPropertiesFunction,
+            attachedPropertiesMetaObject,
             aType->parserStatusCast(), // int parserStatusCast;
             aType->propertyValueSourceCast(), // int valueSourceCast;
             aType->propertyValueInterceptorCast(), // int valueInterceptorCast;
@@ -189,6 +281,8 @@ HarbourPluginLoader::Private::reRegisterType(
     }
 }
 
+#endif // QT_VERSION >= 0x050000
+
 // ==========================================================================
 // HarbourPluginLoader
 // ==========================================================================
@@ -197,9 +291,15 @@ HarbourPluginLoader::HarbourPluginLoader(
     QQmlEngine* aEngine,
     QString aModule,
     int aMajor,
-    int aMinor) :
-    iPrivate(new Private(aEngine, aModule, aMajor, aMinor))
-{}
+    int aMinor)
+{
+#if QT_VERSION >= 0x050000
+    iPrivate = new Private(aEngine, aModule, aMajor, aMinor);
+#else
+    iPrivate = NULL;
+#pragma message "HarbourPluginLoader only supports Qt5"
+#endif
+}
 
 HarbourPluginLoader::~HarbourPluginLoader()
 {
@@ -213,8 +313,10 @@ HarbourPluginLoader::reRegisterType(
     int aMajor,
     int aMinor)
 {
+#if QT_VERSION >= 0x050000
     // Re-register with the same type name (in different module)
     iPrivate->reRegisterType(aQmlName, aModule, aMajor, aMinor);
+#endif
 }
 
 void HarbourPluginLoader::reRegisterType(
@@ -224,12 +326,18 @@ void HarbourPluginLoader::reRegisterType(
     int aMajor,
     int aMinor)
 {
+#if QT_VERSION >= 0x050000
     // Re-register with a different type name (and a module)
     iPrivate->reRegisterType(aOrigName, aNewName, aModule, aMajor, aMinor);
+#endif
 }
 
 bool
 HarbourPluginLoader::isValid() const
 {
+#if QT_VERSION >= 0x050000
     return iPrivate->iLoaded;
+#else
+    return false;
+#endif
 }
