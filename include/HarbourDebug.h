44300017733d9fe5ff98e8879aa7f4f3a65e7a36
Slava Monich <slava.monich@jolla.com>
2019-09-21 13:48:34 +0300

    Added TODO macro

--- include/HarbourDebug.h
+++ include/HarbourDebug.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015-2016 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2019 Jolla Ltd.
+ * Copyright (C) 2015-2019 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,15 +8,15 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of Jolla Ltd nor the names of its contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -72,6 +72,12 @@
 # define HWARN(x) qWarning() << x
 #else
 # define HWARN(x) qWarning() << Q_FUNC_INFO << x
+#endif
+
+// Usage: TODO("Remember to fix this")
+#ifndef TODO
+#  define HARBOUR_PRAGMA(x) _Pragma(#x)
+#  define TODO(x) HARBOUR_PRAGMA(message("TODO - " x))
 #endif
 
 #endif // HARBOUR_DEBUG_H
