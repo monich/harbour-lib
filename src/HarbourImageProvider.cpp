b1cd6f2ae50f48061fce6cd537c0fc160e682953
Slava Monich <slava.monich@jolla.com>
2019-11-09 18:32:06 +0300

    Handle incomplete image size in HarbourImageProvider
    If only one dimension is specified, the image gets loaded twice,
    second time scaled to the specified dimension. Reloading is necessary
    for SVGs (and probably only for SVGs), to make sure that they get
    rendered according to the specified pixel resolution. That's not a
    big deal though, since this provider is mostly being used to load
    SVGs.

--- src/HarbourImageProvider.cpp
+++ src/HarbourImageProvider.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2018 Jolla Ltd.
- * Copyright (C) 2018 Slava Monich <slava@monich.com>
+ * Copyright (C) 2018-2019 Jolla Ltd.
+ * Copyright (C) 2018-2019 Slava Monich <slava@monich.com>
  *
- * You may use this file under the terms of BSD license as follows:
+ * You may use this file under the terms of the BSD license as follows:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,23 +11,24 @@
  *   1. Redistributions of source code must retain the above copyright
  *      notice, this list of conditions and the following disclaimer.
  *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
  *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "HarbourImageProvider.h"
@@ -82,11 +83,31 @@ QImage
 HarbourImageProvider::TextureFactory::load() const
 {
     if (iImage.isNull() && !iPath.isEmpty()) {
-        QImageReader imageReader(iPath);
-        if (iRequestedSize.width() > 0 && iRequestedSize.height() > 0) {
+        if (iRequestedSize.isEmpty()) {
+            if (iImage.load(iPath) && !iImage.size().isEmpty()) {
+                QSize size(iImage.size());
+                if (iRequestedSize.width() > 0) {
+                    size.setHeight(qRound(iRequestedSize.width()*
+                        (qreal)size.height()/size.width()));
+                    size.setWidth(iRequestedSize.width());
+                } else {
+                    size.setWidth(qRound(iRequestedSize.height()*
+                        (qreal)size.width()/size.height()));
+                    size.setHeight(iRequestedSize.height());
+                }
+                if (iImage.size() != size) {
+                    HDEBUG(iImage.size() << "=>" << size);
+                    QImageReader imageReader(iPath);
+                    imageReader.setScaledSize(size);
+                    imageReader.read(&iImage);
+                }
+            }
+        } else {
+            QImageReader imageReader(iPath);
             imageReader.setScaledSize(iRequestedSize);
+            imageReader.read(&iImage);
         }
-        if (imageReader.read(&iImage) && !iImage.isNull()) {
+        if (!iImage.isNull()) {
             if (iHighlight.isEmpty()) {
                 // Colorize grayscale to match ambience
                 if (iImage.isGrayscale()) {
