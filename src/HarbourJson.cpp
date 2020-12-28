48386023bbefdefe6f4cd1aaaa81242b86fcfcb3
Slava Monich <slava.monich@jolla.com>
2020-12-28 18:11:40 +0200

    Restore owner and mode when overwriting JSON file

--- src/HarbourJson.cpp
+++ src/HarbourJson.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2020 Jolla Ltd.
+ * Copyright (C) 2015-2020 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,31 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of Nemo Mobile nor the names of its contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourJson.h"
@@ -46,35 +50,64 @@
 #  include <qjson/serializer.h>
 #endif
 
-bool HarbourJson::save(QString aPath, const QVariantMap& aMap)
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+bool HarbourJson::save(const QString& aPath, const QVariantMap& aMap)
 {
     QFileInfo file(aPath);
     QDir dir(file.dir());
     if (dir.mkpath(dir.absolutePath())) {
-        QFile f(file.absoluteFilePath());
+        const QString absPath(file.absoluteFilePath());
+        QFile f(absPath);
         if (!aMap.isEmpty()) {
+            struct stat st;
+            const QByteArray pathBytes(absPath.toLocal8Bit());
+            const char* path = pathBytes.constData();
+            const bool haveFileAttr = (stat(path, &st) == 0);
+
             if (f.open(QIODevice::WriteOnly)) {
+                bool ok;
 #if QT_VERSION >= 0x050000
                 if (f.write(QJsonDocument::fromVariant(aMap).toJson()) >= 0) {
-                    return true;
+                    ok = true;
+                } else {
+                    HWARN("Error writing" << absPath << f.errorString());
                 }
 #else
                 QJson::Serializer serializer;
                 QByteArray json = serializer.serialize(aMap);
                 if (!json.isNull()) {
-                    if (f.write(json ) >= 0) {
-                        return true;
+                    if (f.write(json) >= 0) {
+                        ok = true;
+                    } else {
+                        HWARN("Error writing" << absPath << f.errorString());
                     }
                 } else {
                     HWARN("Json serialization error");
                 }
 #endif
-                HWARN("Error writing" << aPath << f.errorString());
+                if (ok) {
+                    if (haveFileAttr) {
+                        // Try to restore ownership and mode
+                        if (chown(path, st.st_uid, st.st_gid)) {
+                            HWARN("Failed to chown" << path << ":" <<
+                                strerror(errno));
+                        }
+                        if (chmod(path, st.st_mode & ~S_IFMT)) {
+                            HWARN("Failed to chmod" << path << ":" <<
+                                strerror(errno));
+                        }
+                    }
+                    return true;
+                }
             } else {
-                HWARN("Error opening" << aPath << f.errorString());
+                HWARN("Error opening" << absPath << f.errorString());
             }
         } else if (!f.remove()) {
-            HWARN("Error removing" << aPath << f.errorString());
+            HWARN("Error removing" << absPath << f.errorString());
         }
     } else {
         HWARN("Failed to create" << dir.absolutePath());
@@ -82,7 +115,7 @@ bool HarbourJson::save(QString aPath, const QVariantMap& aMap)
     return false;
 }
 
-bool HarbourJson::load(QString aPath, QVariantMap& aRoot)
+bool HarbourJson::load(const QString& aPath, QVariantMap& aRoot)
 {
     QFile f(aPath);
     if (f.exists()) {
