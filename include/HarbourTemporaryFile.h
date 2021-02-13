238b8a0c1227b81be102689991d7b9a4de9411e8
Slava Monich <slava.monich@jolla.com>
2021-02-13 05:25:36 +0200

    Allow to choose a location for HarbourTemporaryFile
    Many system apps in Sailfish OS 4.0 have lost access to /tmp
    That's supposed to be some sort of security enhancement.

--- include/HarbourTemporaryFile.h
+++ include/HarbourTemporaryFile.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2018 Jolla Ltd.
- * Copyright (C) 2018 Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2018-2021 Jolla Ltd.
+ * Copyright (C) 2018-2021 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,31 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   - Neither the name of Jolla Ltd nor the names of its contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #ifndef HARBOUR_TEMPORARY_FILE_H
@@ -44,17 +48,27 @@ class HarbourTemporaryFile: public QObject
     Q_PROPERTY(QString fileTemplate READ fileTemplate WRITE setFileTemplate NOTIFY fileTemplateChanged)
     Q_PROPERTY(QString content READ content WRITE setContent NOTIFY contentChanged)
     Q_PROPERTY(QString fileName READ fileName NOTIFY fileNameChanged)
+    Q_PROPERTY(Location location READ location WRITE setLocation NOTIFY locationChanged)
     Q_PROPERTY(QUrl url READ url NOTIFY urlChanged)
+    Q_ENUMS(Location)
 
 public:
+    enum Location {
+        Tmp,
+        Downloads
+    };
+
     explicit HarbourTemporaryFile(QObject* aParent = NULL);
     ~HarbourTemporaryFile();
 
     QString content() const;
-    void setContent(QString value);
+    void setContent(QString aValue);
 
     QString fileTemplate() const;
-    void setFileTemplate(QString value);
+    void setFileTemplate(QString aValue);
+
+    Location location() const;
+    void setLocation(Location aValue);
 
     QString fileName() const;
     QUrl url() const;
@@ -63,6 +77,7 @@ Q_SIGNALS:
     void fileTemplateChanged();
     void contentChanged();
     void fileNameChanged();
+    void locationChanged();
     void urlChanged();
 
 private:
--- src/HarbourTemporaryFile.cpp
+++ src/HarbourTemporaryFile.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2018 Jolla Ltd.
- * Copyright (C) 2018 Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2018-2021 Jolla Ltd.
+ * Copyright (C) 2018-2021 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,31 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   - Neither the name of Jolla Ltd nor the names of its contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourTemporaryFile.h"
@@ -37,6 +41,7 @@
 #include <QDir>
 #include <QTemporaryFile>
 #include <QTextStream>
+#include <QStandardPaths>
 
 // ==========================================================================
 // HarbourTemporaryFile::Private
@@ -48,9 +53,11 @@ public:
     ~Private();
 
     void reopen(HarbourTemporaryFile* aObject);
+    QString directoryPath() const;
     QString fileName() const;
 
 public:
+    Location iLocation;
     QTemporaryFile* iFile;
     QString iFileTemplate;
     QString iContent;
@@ -58,7 +65,8 @@ public:
 };
 
 HarbourTemporaryFile::Private::Private() :
-   iFile(NULL)
+    iLocation(Tmp),
+    iFile(NULL)
 {
 }
 
@@ -76,6 +84,21 @@ HarbourTemporaryFile::Private::fileName() const
     return iFile ? iFile->fileName() : QString();
 }
 
+QString
+HarbourTemporaryFile::Private::directoryPath() const
+{
+    QStandardPaths::StandardLocation type = QStandardPaths::TempLocation;
+    switch (iLocation) {
+    case Downloads:
+        type = QStandardPaths::DownloadLocation;
+        break;
+    case Tmp:
+        // This is the default
+        break;
+    }
+    return QStandardPaths::writableLocation(type);
+}
+
 void
 HarbourTemporaryFile::Private::reopen(
     HarbourTemporaryFile* aObject)
@@ -89,7 +112,7 @@ HarbourTemporaryFile::Private::reopen(
         iUrl = QUrl();
     }
     if (!iContent.isEmpty() && !iFileTemplate.isEmpty()) {
-        iFile = new QTemporaryFile(QDir::tempPath() +
+        iFile = new QTemporaryFile(directoryPath() +
             QDir::separator() + iFileTemplate);
         iFile->setAutoRemove(true);
         if (iFile->open()) {
@@ -156,6 +179,22 @@ HarbourTemporaryFile::setFileTemplate(QString aValue)
         iPrivate->iFileTemplate = aValue;
         iPrivate->reopen(this);
         Q_EMIT fileTemplateChanged();
+    }
+}
+
+HarbourTemporaryFile::Location
+HarbourTemporaryFile::location() const
+{
+    return iPrivate->iLocation;
+}
+
+void
+HarbourTemporaryFile::setLocation(Location aValue)
+{
+    if (iPrivate->iLocation != aValue) {
+        iPrivate->iLocation = aValue;
+        iPrivate->reopen(this);
+        Q_EMIT locationChanged();
     }
 }
 
