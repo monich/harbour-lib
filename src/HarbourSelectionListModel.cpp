73ab079dea1d23bd9f7f5fc37d5a9e250f8cf26f
Slava Monich <slava@monich.com>
2024-03-25 03:03:53 +0200

    Added HarbourSelectionListModel::selectedValues()

--- src/HarbourSelectionListModel.cpp
+++ src/HarbourSelectionListModel.cpp
@@ -1,6 +1,6 @@
 ﻿/*
  * Copyright (C) 2019 Jolla Ltd.
- * Copyright (C) 2019-2023 Slava Monich <slava@jolla.com>
+ * Copyright (C) 2019-2024 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,33 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in
- *      the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourSelectionListModel.h"
@@ -68,13 +74,14 @@ public:
 
     static int binaryFind(const QList<int>, int);
 
-    HarbourSelectionListModel* parentModel();
+    HarbourSelectionListModel* parentModel() const;
     void queueSignal(Signal aSignal);
     void emitQueuedSignals();
     bool isSelectionRole(int) const;
     bool isSelectedRow(int) const;
     bool isSelectableRow(int) const;
     int findSelectedRow(int) const;
+    int findRole(const QString&) const;
     void updateCounts();
     void selectRow(int);
     void unselectRow(int);
@@ -141,7 +148,7 @@ HarbourSelectionListModel::Private::binaryFind(
 
 inline
 HarbourSelectionListModel*
-HarbourSelectionListModel::Private::parentModel()
+HarbourSelectionListModel::Private::parentModel() const
 {
     return qobject_cast<HarbourSelectionListModel*>(parent());
 }
@@ -217,6 +224,24 @@ HarbourSelectionListModel::Private::findSelectedRow(
     int aRow) const
 {
     return binaryFind(iSelectedRows, aRow);
+}
+
+int
+HarbourSelectionListModel::Private::findRole(
+    const QString& aRole) const
+{
+    if (!aRole.isEmpty()) {
+        const QByteArray roleName(aRole.toUtf8());
+        QHashIterator<int,QByteArray> roles(parentModel()->roleNames());
+        while (roles.hasNext()) {
+            roles.next();
+            if (roles.value() == roleName) {
+                return roles.key();
+            }
+        }
+        HDEBUG("Unknown role" << aRole);
+    }
+    return -1;
 }
 
 void
@@ -519,6 +544,25 @@ HarbourSelectionListModel::toggleRows(
     const QList<int> aRows)
 {
     iPrivate->toggleRows(aRows);
+}
+
+QVariantList
+HarbourSelectionListModel::selectedValues(
+    QString aRole) const
+{
+    QVariantList values;
+    const int n = iPrivate->iSelectedRows.count();
+    if (n > 0) {
+        const int role = iPrivate->findRole(aRole);
+        if (role >= 0) {
+            values.reserve(n);
+            for (int i = 0; i < n; i++) {
+                values.append(data(index(iPrivate->iSelectedRows.at(i), 0), role));
+            }
+        }
+    }
+    HDEBUG(aRole << values);
+    return values;
 }
 
 Qt::ItemFlags
