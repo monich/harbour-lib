0fb12d90e5d899539a4987cb20156262d4e1cf52
Slava Monich <slava@monich.com>
2024-09-16 02:40:15 +0300

    Allow HarbourSingleImageProvider to mirror the image

--- include/HarbourSingleImageProvider.h
+++ include/HarbourSingleImageProvider.h
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2019-2024 Slava Monich <slava@monich.com>
  * Copyright (C) 2019 Jolla Ltd.
- * Copyright (C) 2019 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,50 +8,77 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in
- *      the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #ifndef HARBOUR_SINGLE_IMAGE_PROVIDER_H
 #define HARBOUR_SINGLE_IMAGE_PROVIDER_H
 
-#include <QImage>
-#include <QObject>
-#include <QQmlParserStatus>
+#include <QtCore/QObject>
+#include <QtGui/QImage>
+#include <QtQml/QQmlParserStatus>
 
-class HarbourSingleImageProvider : public QObject, public QQmlParserStatus {
+class HarbourSingleImageProvider :
+    public QObject,
+    public QQmlParserStatus
+{
     Q_OBJECT
     Q_INTERFACES(QQmlParserStatus)
+    Q_PROPERTY(bool mirrorHorizontally READ mirrorHorizontally WRITE setMirrorHorizontally NOTIFY mirrorHorizontallyChanged)
+    Q_PROPERTY(bool mirrorVertically READ mirrorVertically WRITE setMirrorVertically NOTIFY mirrorVerticallyChanged)
     Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
     Q_PROPERTY(QString source READ source NOTIFY sourceChanged)
+    Q_FLAGS(Options)
 
 public:
+    enum Option {
+        Default = 0,
+        MirrorHorizontally = 0x01,
+        MirrorVertically = 0x02
+    };
+
+    Q_DECLARE_FLAGS(Options, Option)
+
     HarbourSingleImageProvider(QObject* aParent = Q_NULLPTR);
 
     QImage image() const;
-    void setImage(QImage aImage);
+    void setImage(QImage);
+
+    bool mirrorHorizontally() const;
+    void setMirrorHorizontally(bool);
+
+    bool mirrorVertically() const;
+    void setMirrorVertically(bool);
 
     QString source() const;
 
+    Q_INVOKABLE void set(QImage, Options);
     Q_INVOKABLE void clear();
 
     // QQmlParserStatus
@@ -59,6 +86,8 @@ public:
     void componentComplete() Q_DECL_OVERRIDE;
 
 Q_SIGNALS:
+    void mirrorHorizontallyChanged();
+    void mirrorVerticallyChanged();
     void imageChanged();
     void sourceChanged();
 
@@ -67,5 +96,7 @@ private:
     class ImageProvider;
     Private* iPrivate;
 };
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(HarbourSingleImageProvider::Options)
 
 #endif // HARBOUR_SINGLE_IMAGE_PROVIDER_H
--- src/HarbourSingleImageProvider.cpp
+++ src/HarbourSingleImageProvider.cpp
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2019-2024 Slava Monich <slava@monich.com>
  * Copyright (C) 2019 Jolla Ltd.
- * Copyright (C) 2019 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,91 +8,121 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in
- *      the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourSingleImageProvider.h"
 
 #include "HarbourDebug.h"
 
-#include <QQuickImageProvider>
-#include <QQmlEngine>
-#include <QQmlContext>
+#include <QtQml/QQmlEngine>
+#include <QtQml/QQmlContext>
+#include <QtQuick/QQuickImageProvider>
 
 // ==========================================================================
 // HarbourSingleImageProvider::ImageProvider
 // ==========================================================================
 
-class HarbourSingleImageProvider::ImageProvider : public QQuickImageProvider {
+class HarbourSingleImageProvider::ImageProvider :
+    public QQuickImageProvider
+{
 public:
     static const QString IMAGE_NAME;
 
-    ImageProvider(QImage aImage);
+    ImageProvider(QImage);
 
-    QImage requestImage(const QString& aId, QSize* aSize,
-        const QSize& aRequested) Q_DECL_OVERRIDE;
+    QImage requestImage(const QString&, QSize*, const QSize&) Q_DECL_OVERRIDE;
 
 private:
-    QImage iImage;
+    const QImage iImage;
+    const QSize iImageSize;
+    const Qt::TransformationMode iTransformationMode;
 };
 
 const QString HarbourSingleImageProvider::ImageProvider::IMAGE_NAME("image");
 
-HarbourSingleImageProvider::ImageProvider::ImageProvider(QImage aImage) :
+HarbourSingleImageProvider::ImageProvider::ImageProvider(
+    QImage aImage) :
     QQuickImageProvider(Image),
-    iImage(aImage)
+    iImage(aImage),
+    iImageSize(aImage.size()),
+    iTransformationMode(Qt::SmoothTransformation)
 {
 }
 
-QImage HarbourSingleImageProvider::ImageProvider::requestImage(const QString& aId,
-    QSize* aSize, const QSize& aRequested)
+QImage
+HarbourSingleImageProvider::ImageProvider::requestImage(
+    const QString& aId,
+    QSize* aSize,
+    const QSize& aRequested)
 {
-    HDEBUG(aId);
+    HDEBUG(aId << aRequested);
     if (aSize) {
-        *aSize = iImage.size();
+        *aSize = iImageSize;
     }
-    return iImage;
+    return (aRequested.isEmpty() || iImageSize == aRequested) ? iImage :
+        iImage.scaled(aRequested, Qt::IgnoreAspectRatio, iTransformationMode);
 }
 
 // ==========================================================================
 // HarbourSingleImageProvider::Private
 // ==========================================================================
 
-class HarbourSingleImageProvider::Private : public QObject {
+class HarbourSingleImageProvider::Private :
+    public QObject
+{
     Q_OBJECT
 
 public:
-    Private(HarbourSingleImageProvider* aParent);
+    class SaveState
+    {
+    public:
+        SaveState(HarbourSingleImageProvider*);
+        void emitSignals(HarbourSingleImageProvider*) const;
+
+    private:
+        const Options iOptions;
+        const QString iId;
+    };
+
+    Private(HarbourSingleImageProvider*);
     ~Private();
 
-    bool setEngine(QQmlEngine* aEngine);
-    bool setImage(QImage aImage);
+    bool setEngine(QQmlEngine*);
+    void set(QImage, Options);
     void registerProvider();
 
 public Q_SLOTS:
     void onEngineDied();
 
 public:
+    Options iOptions;
     QImage iImage;
     QString iId;
     QString iSourceUri;
@@ -101,6 +131,7 @@ public:
 
 HarbourSingleImageProvider::Private::Private(HarbourSingleImageProvider* aParent) :
     QObject(aParent),
+    iOptions(Default),
     iEngine(Q_NULLPTR)
 {
 }
@@ -112,7 +143,8 @@ HarbourSingleImageProvider::Private::~Private()
     }
 }
 
-void HarbourSingleImageProvider::Private::registerProvider()
+void
+HarbourSingleImageProvider::Private::registerProvider()
 {
     QQmlImageProviderBase* provider = new ImageProvider(iImage);
     iId = QString().sprintf("HarbourSingleImageProvider%p", provider);
@@ -121,7 +153,9 @@ void HarbourSingleImageProvider::Private::registerProvider()
     iEngine->addImageProvider(iId, provider);
 }
 
-bool HarbourSingleImageProvider::Private::setEngine(QQmlEngine* aEngine)
+bool
+HarbourSingleImageProvider::Private::setEngine(
+    QQmlEngine* aEngine)
 {
     if (iEngine) {
         if (!iId.isEmpty()) {
@@ -141,42 +175,80 @@ bool HarbourSingleImageProvider::Private::setEngine(QQmlEngine* aEngine)
     return false;
 }
 
-bool HarbourSingleImageProvider::Private::setImage(QImage aImage)
+void
+HarbourSingleImageProvider::Private::set(
+    QImage aImage,
+    Options aOptions)
 {
-    HDEBUG(aImage);
+    const bool mirrorHorizontally = aOptions.testFlag(MirrorHorizontally);
+    const bool mirrorVertically = aOptions.testFlag(MirrorVertically);
+    HDEBUG(aImage << mirrorHorizontally << mirrorVertically);
     if (!iId.isEmpty()) {
         iEngine->removeImageProvider(iId);
     }
-    iImage = aImage;
+    iOptions = aOptions;
+    iImage = (aImage.width() && (mirrorHorizontally | mirrorVertically)) ?
+        aImage.mirrored(mirrorHorizontally, mirrorVertically) : aImage;
     if (iEngine && !iImage.isNull()) {
         registerProvider();
-        return true;
     }
-    return false;
 }
 
-void HarbourSingleImageProvider::Private::onEngineDied()
+void
+HarbourSingleImageProvider::Private::onEngineDied()
 {
     HDEBUG("engine died");
     iEngine = Q_NULLPTR;
     iId = QString();
 }
 
+
+// ==========================================================================
+// HarbourSingleImageProvider::Private::SaveState
+// ==========================================================================
+
+HarbourSingleImageProvider::Private::SaveState::SaveState(
+    HarbourSingleImageProvider* aObject) :
+    iOptions(aObject->iPrivate->iOptions),
+    iId(aObject->iPrivate->iId)
+{}
+
+void
+HarbourSingleImageProvider::Private::SaveState::emitSignals(
+    HarbourSingleImageProvider* aObject) const
+{
+    Private* priv = aObject->iPrivate;
+    const Options newOpts(priv->iOptions);
+
+    if (iId != aObject->iPrivate->iId) {
+        Q_EMIT aObject->sourceChanged();
+    }
+    if ((iOptions & MirrorHorizontally) != (newOpts & MirrorHorizontally)) {
+        Q_EMIT aObject->mirrorHorizontallyChanged();
+    }
+    if ((iOptions & MirrorVertically) != (newOpts & MirrorVertically)) {
+        Q_EMIT aObject->mirrorVerticallyChanged();
+    }
+}
+
 // ==========================================================================
 // HarbourSingleImageProvider
 // ==========================================================================
 
-HarbourSingleImageProvider::HarbourSingleImageProvider(QObject* aParent) :
+HarbourSingleImageProvider::HarbourSingleImageProvider(
+    QObject* aParent) :
     QObject(aParent),
     iPrivate(new Private(this))
 {
 }
 
-void HarbourSingleImageProvider::classBegin()
+void
+HarbourSingleImageProvider::classBegin()
 {
 }
 
-void HarbourSingleImageProvider::componentComplete()
+void
+HarbourSingleImageProvider::componentComplete()
 {
     QQmlContext* context = QQmlEngine::contextForObject(this);
     HDEBUG(context);
@@ -189,30 +261,82 @@ void HarbourSingleImageProvider::componentComplete()
     }
 }
 
-QImage HarbourSingleImageProvider::image() const
+QImage
+HarbourSingleImageProvider::image() const
 {
     return iPrivate->iImage;
 }
 
-void HarbourSingleImageProvider::setImage(QImage aImage)
+void
+HarbourSingleImageProvider::setImage(QImage aImage)
 {
+    const Private::SaveState state(this);
     HDEBUG(aImage);
-    if (iPrivate->setImage(aImage)) {
-        Q_EMIT sourceChanged();
-    }
-    Q_EMIT imageChanged();
+    iPrivate->set(aImage, iPrivate->iOptions);
+    Q_EMIT imageChanged(); // Not emitted by SaveState
+    state.emitSignals(this);
 }
 
-QString HarbourSingleImageProvider::source() const
+bool
+HarbourSingleImageProvider::mirrorHorizontally() const
+{
+    return iPrivate->iOptions.testFlag(MirrorHorizontally);
+}
+
+void
+HarbourSingleImageProvider::setMirrorHorizontally(
+    bool aMirrorHorizontally)
+{
+    if (mirrorHorizontally() != aMirrorHorizontally) {
+        const Private::SaveState state(this);
+        HDEBUG(aMirrorHorizontally);
+        iPrivate->set(iPrivate->iImage, iPrivate->iOptions ^ MirrorHorizontally);
+        state.emitSignals(this);
+    }
+}
+
+bool
+HarbourSingleImageProvider::mirrorVertically() const
+{
+    return iPrivate->iOptions.testFlag(MirrorVertically);
+}
+
+void
+HarbourSingleImageProvider::setMirrorVertically(
+    bool aMirrorVertically)
+{
+    if (mirrorVertically() != aMirrorVertically) {
+        const Private::SaveState state(this);
+        HDEBUG(aMirrorVertically);
+        iPrivate->set(iPrivate->iImage, iPrivate->iOptions ^ MirrorVertically);
+        state.emitSignals(this);
+    }
+}
+
+QString
+HarbourSingleImageProvider::source() const
 {
     return iPrivate->iSourceUri;
 }
 
-void HarbourSingleImageProvider::clear()
+void
+HarbourSingleImageProvider::set(
+    QImage aImage,
+    Options aOptions)
+{
+    const Private::SaveState state(this);
+    HDEBUG(aImage << aOptions);
+    iPrivate->set(aImage, aOptions);
+    Q_EMIT imageChanged(); // Not emitted by SaveState
+    state.emitSignals(this);
+}
+
+void
+HarbourSingleImageProvider::clear()
 {
     if (!iPrivate->iImage.isNull()) {
         HDEBUG("clearing image");
-        iPrivate->setImage(QImage());
+        iPrivate->set(QImage(), iPrivate->iOptions);
         Q_EMIT sourceChanged();
         Q_EMIT imageChanged();
     }
