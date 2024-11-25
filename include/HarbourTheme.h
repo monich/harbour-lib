6f7d1d49bd01aa286654bc43b09334848ff68581
Slava Monich <slava@monich.com>
2024-11-25 17:43:56 +0200

    Removed unused field
    Found by Coverity scan

--- include/HarbourTheme.h
+++ include/HarbourTheme.h
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2018-2024 Slava Monich <slava@monich.com>
  * Copyright (C) 2018-2022 Jolla Ltd.
- * Copyright (C) 2018-2022 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,39 +8,46 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer
- *      in the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #ifndef HARBOUR_THEME_H
 #define HARBOUR_THEME_H
 
-#include <QObject>
-#include <QColor>
+#include <QtCore/QObject>
+#include <QtGui/QColor>
 
 class QQmlEngine;
 class QJSEngine;
 
-class HarbourTheme : public QObject
+class HarbourTheme :
+    public QObject
 {
     Q_OBJECT
     Q_ENUMS(ColorScheme)
@@ -68,7 +75,7 @@ public:
     ~HarbourTheme();
 
     // Callback for qmlRegisterSingletonType<HarbourTheme>
-    static QObject* createSingleton(QQmlEngine* aEngine, QJSEngine* aScript);
+    static QObject* createSingleton(QQmlEngine*, QJSEngine*);
 
     QColor invertedPrimaryColor() const;
     QColor primaryColor() const;
@@ -81,15 +88,12 @@ public:
     qreal opacityHigh() const;
     qreal opacityOverlay() const;
 
-    Q_INVOKABLE static QColor invertedColor(const QColor& aColor);
-    Q_INVOKABLE static qreal colorDifference(const QColor& aColor1, const QColor& aColor2);
+    Q_INVOKABLE static QColor invertedColor(const QColor&);
+    Q_INVOKABLE static qreal colorDifference(const QColor&, const QColor&);
 
 Q_SIGNALS:
     void colorSchemeChanged();
     void primaryColorChanged();
-
-private:
-    QObject* iPrivate;
 };
 
 #endif // HARBOUR_THEME_H
