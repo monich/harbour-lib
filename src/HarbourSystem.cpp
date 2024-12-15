7389c0f3e90950f45227e165ebb6e40a77175865
Slava Monich <slava@monich.com>
2024-12-15 05:14:35 +0200

    Let dlopen() to locate the libs
    For the libraries located in the standard places dlopen() should
    be smart enough to figure it out. The actual location of the shared
    libraries may be distro specific, hardcoding it doesn't seem to be
    a good idea.

--- src/HarbourSystem.cpp
+++ src/HarbourSystem.cpp
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2020-2024 Slava Monich <slava@monich.com>
  * Copyright (C) 2020 Jolla Ltd.
- * Copyright (C) 2020 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,60 +8,55 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer
- *      in the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourSystem.h"
 #include "HarbourDebug.h"
 
-#include <stdlib.h>
-#include <string.h>
 #include <dlfcn.h>
 
-#ifdef LIBDIR
-#  define HARBOUR_LIBDIR LIBDIR
-#elif defined __aarch64__
-#  define HARBOUR_LIBDIR "/usr/lib64"
-#else
-#  define HARBOUR_LIBDIR "/usr/lib"
-#endif
-
-void* HarbourDlopen(const char* aLibFile, int aFlags)
+/*
+ * This function used to pick the specific directory where to look for the
+ * library based on the compiler architecture, but that doesn't seem to be
+ * necessary. For the libraries located in the standard places dlopen()
+ * should be smart enough to figure it out.
+ */
+void*
+HarbourDlopen(
+    const char* aLibFile,
+    int aFlags)
 {
-    const char* libdir = HARBOUR_LIBDIR;
-    const size_t libdir_len = strlen(libdir);
-    const size_t libfile_len = strlen(aLibFile);
-    char* filename = (char*) malloc(libdir_len + libfile_len + 2);
-    void* handle;
+    void* handle = dlopen(aLibFile, aFlags);
 
-    memcpy(filename, libdir, libdir_len);
-    memcpy(filename + libdir_len + 1, aLibFile, libfile_len);
-    filename[libdir_len] = '/';
-    filename[libdir_len + 1 + libfile_len] = '\0';
-    handle = dlopen(filename, aFlags);
     if (!handle) {
-        HWARN("Failed to load" << filename);
+        HWARN("Failed to load" << aLibFile);
     }
-    free(filename);
     return handle;
 }
