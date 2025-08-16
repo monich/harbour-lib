2e8f0672bdb6aa9763f5a85e9a3d9a11356ca7dc
Slava Monich <slava@monich.com>
2025-08-17 02:20:11 +0300

    Added missign const qualifier

--- src/HarbourBase32.cpp
+++ src/HarbourBase32.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2019-2022 Jolla Ltd.
- * Copyright (C) 2019-2022 Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2019-2025 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,33 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in
- *      the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourBase32.h"
@@ -53,6 +59,7 @@ public:
     static int base32ToNibble(char);
 };
 
+// static
 inline
 char
 HarbourBase32::Private::nibbleToBase32(
@@ -62,6 +69,7 @@ HarbourBase32::Private::nibbleToBase32(
     return (aNibble < 26) ? (aBaseChar + aNibble) : ('2' + (aNibble - 26));
 }
 
+// static
 inline
 int
 HarbourBase32::Private::base32ToNibble(
@@ -82,9 +90,10 @@ HarbourBase32::Private::base32ToNibble(
 // HarbourBase32
 // ==========================================================================
 
+// static
 bool
 HarbourBase32::isValidBase32(
-    QString aBase32)
+    const QString aBase32)
 {
     const int n = aBase32.length();
     const QChar* chars = aBase32.constData();
@@ -143,6 +152,7 @@ HarbourBase32::isValidBase32(
     return !empty;
 }
 
+// static
 QByteArray
 HarbourBase32::fromBase32(
     const QString aBase32)
@@ -217,6 +227,7 @@ HarbourBase32::fromBase32(
     return out;
 }
 
+// static
 QString
 HarbourBase32::toBase32(
     const QByteArray aBinary,
