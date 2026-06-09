9c9de9d2a3e6d9e86dcba3a24fa613973772d80d
Slava Monich <slava@monich.com>
2026-06-09 03:17:39 +0300

    HarbourProcessState housekeeping

--- include/HarbourProcessState.h
+++ include/HarbourProcessState.h
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2021-2026 Slava Monich <slava@monich.com>
  * Copyright (C) 2021 Jolla Ltd.
- * Copyright (C) 2021 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,41 +8,47 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer
- *      in the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #ifndef HARBOUR_PROCESS_STATE_H
 #define HARBOUR_PROCESS_STATE_H
 
-#include <QObject>
+#include <QtCore/QObject>
 
 class QQmlEngine;
 class QJSEngine;
 
-class HarbourProcessState: public QObject
+class HarbourProcessState :
+    public QObject
 {
     Q_OBJECT
-    Q_DISABLE_COPY(HarbourProcessState)
     Q_PROPERTY(bool jailedApp READ isJailedApp CONSTANT)
     Q_PROPERTY(bool privileged READ isPrivileged CONSTANT)
 
@@ -50,7 +56,7 @@ public:
     explicit HarbourProcessState(QObject* aParent = Q_NULLPTR);
 
     // Callback for qmlRegisterSingletonType<HarbourProcessState>
-    static QObject* createSingleton(QQmlEngine* aEngine, QJSEngine* aScript);
+    static QObject* createSingleton(QQmlEngine*, QJSEngine*);
 
     static bool isJailedApp();
     static bool isPrivileged();
--- src/HarbourProcessState.cpp
+++ src/HarbourProcessState.cpp
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2021-2026 Slava Monich <slava@monich.com>
  * Copyright (C) 2021 Jolla Ltd.
- * Copyright (C) 2021 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,33 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer
- *      in the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "HarbourProcessState.h"
@@ -38,18 +44,23 @@
 #include <grp.h>
 #include <sys/types.h>
 
-HarbourProcessState::HarbourProcessState(QObject* aParent) :
+HarbourProcessState::HarbourProcessState(
+    QObject* aParent) :
     QObject(aParent)
-{
-}
+{}
 
 // Callback for qmlRegisterSingletonType<HarbourProcessState>
-QObject* HarbourProcessState::createSingleton(QQmlEngine*, QJSEngine*)
+QObject*
+HarbourProcessState::createSingleton(
+    QQmlEngine*,
+    QJSEngine*)
 {
-    return new HarbourProcessState;
+    return new HarbourProcessState(); // Singleton doesn't need a parent
 }
 
-bool HarbourProcessState::isJailedApp()
+// static
+bool
+HarbourProcessState::isJailedApp()
 {
     static int processJailed = -1;
 
@@ -60,6 +71,7 @@ bool HarbourProcessState::isJailedApp()
     if (processJailed < 0) {
         bool simulate = false;
         const char* var = "HARBOUR_JAIL_STATE";
+
         processJailed = QString(qgetenv(var)).toInt(&simulate);
         if (!simulate || processJailed < 0) {
             if (getppid() == 1) {
@@ -76,7 +88,9 @@ bool HarbourProcessState::isJailedApp()
     return processJailed > 0;
 }
 
-bool HarbourProcessState::isPrivileged()
+// static
+bool
+HarbourProcessState::isPrivileged()
 {
     static gid_t privilegedGid = 0;
 
@@ -88,7 +102,9 @@ bool HarbourProcessState::isPrivileged()
             HDEBUG("privileged =" << privilegedGid);
         }
     }
+
     const gid_t egid = getegid();
+
     if (egid == privilegedGid) {
         HDEBUG("Yes, we are privileged :)");
         return true;
